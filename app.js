82c3ff103a22c3980a771137ff42d8f97197cc00
Pedro Ferreira <pedroferrodude@hotmail.com>
2021-08-27 13:16:07 +0100

    First iteration

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
 
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then((data) => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch((error) =>
+    console.log('Something went wrong when retrieving an access token', error)
+  );
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -15,4 +28,6 @@ app.use(express.static(__dirname + '/public'));
 
 // Our routes go here:
 
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.listen(3000, () =>
+  console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊')
+);
